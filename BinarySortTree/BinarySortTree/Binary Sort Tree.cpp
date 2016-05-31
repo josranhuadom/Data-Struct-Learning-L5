8ef68b2577219cfff93f30a4c9eef27da673127d
josranhuadom <Jkjoshuaking@hotmail.com>
2016-05-31 22:59:16 +0800

    b

--- BinarySortTree/BinarySortTree/Binary Sort Tree.cpp
+++ BinarySortTree/BinarySortTree/Binary Sort Tree.cpp
@@ -1,31 +1,55 @@
-typedef int KeyType
-typedef int InfoType
+#include<iostream>
+using namespace std;
+
+typedef int KeyType;
+typedef int InfoType;
 
 typedef struct
 {
 	KeyType key;
 	InfoType otherinfo;
 }ElemType;
-typedef struct BSTNode//能删掉吗？
+typedef struct BSTNode
 {
 	ElemType data;
 	struct BSTNode *lchild, *rchild;
-}BSTNode, *BSTree;
+}BSTNode,  BSTree;
 
-void CreatBST(BSTree &T)
+ElemType e;
+
+void InsertBST(BSTree *&T, ElemType e)
+{
+	if (!T)
+	{
+		BSTNode * S = new BSTNode;
+		S->data = e;
+		S->lchild = S->rchild = NULL;
+		T = S;
+	}
+	else if (e.key < T->data.key)
+		InsertBST(T->lchild, e);
+	else if (e.key > T->data.key)
+		InsertBST(T->rchild, e);
+}
+
+void CreatBST(BSTree *&T)
 {
 	T = NULL;
-	cin >> e;
-	if(e.key == '#')
-		break;
+	cin >> e.key;
+	if (e.key == '#')
+	{
+		cout << "没有建立数组" << endl;
+		return;
+	}
 	else
 	{
 		InsertBST(T, e);
-		cin >> e;
+		cin >> e.key;
 	}
 }
 
-void InsertBST(BSTree &T, ElemType e)
+int main()
 {
-	
+	BSTree *T = new BSTree;
+	CreatBST(T);
 }
