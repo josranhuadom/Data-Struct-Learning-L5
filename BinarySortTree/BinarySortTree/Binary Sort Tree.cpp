41bbc9dab1875a90e8be70f6d271d6dda344b02b
josranhuadom <Jkjoshuaking@hotmail.com>
2016-06-05 16:14:22 +0800

    a

--- BinarySortTree/BinarySortTree/Binary Sort Tree.cpp
+++ BinarySortTree/BinarySortTree/Binary Sort Tree.cpp
@@ -1,9 +1,11 @@
+//二叉排序树
 #include<iostream>
 using namespace std;
 
 typedef int KeyType;
 typedef int InfoType;
 
+//二叉排序树的二叉链表存储表示
 typedef struct
 {
 	KeyType key;
@@ -15,21 +17,22 @@ typedef struct BSTNode
 	struct BSTNode *lchild, *rchild;
 }BSTNode,  BSTree;
 
-ElemType e;
+ElemType e;    //新建结点e以储存输入的值
 
+//当二叉排序树 T 中不存在关键字等于 e.key 的数据元素是，则插入该元素
 void InsertBST(BSTree *&T, ElemType e)
 {
 	if (!T)
-	{
-		BSTNode * S = new BSTNode;
-		S->data = e;
-		S->lchild = S->rchild = NULL;
-		T = S;
+	{//这是找到插入位置的情况，此时是最后一次进行递归调用
+		BSTNode * S = new BSTNode;      //生成新节点 *S
+		S->data = e;                                      //新节点 *S 的数据域置为 e
+		S->lchild = S->rchild = NULL;       //新节点 *S 作为叶子节点
+		T = S;                                                  //把新节点 *S 链接到已找到的插入位置
 	}
 	else if (e.key < T->data.key)
-		InsertBST(T->lchild, e);
+		InsertBST(T->lchild, e);                   //将 *S 插入左子树
 	else if (e.key > T->data.key)
-		InsertBST(T->rchild, e);
+		InsertBST(T->rchild, e);                  //将 *S 插入右子树
 }
 
 void CreatBST(BSTree *&T)
